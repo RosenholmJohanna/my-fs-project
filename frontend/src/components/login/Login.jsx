b87660ed8a186bbbc94abb0f34fe533010e90961
rosenholmjohanna <rosenholm.johanna88@gmail.com>
2024-02-16 23:31:31 +0100

    be test

---
+++ frontend/src/components/login/Login.jsx
@@ -0,0 +1,16 @@
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector} from 'react-redux' //batch deprecated, enabled in reactDom 18
+//import { useNavigate, Link} from 'react-router-dom'
+
+
+const Login = () => {
+
+  return (
+    <>
+    <p>Hello !</p>
+    </>
+    );
+
+}
+
+export default Login
